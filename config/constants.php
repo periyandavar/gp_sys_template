e4175006ecbb00a329a2b61b96f473cc444109e0
Periyandavar Ganesh <vickyperiyandavar@gmail.com>
2025-03-23 12:04:24 +0530

    feat: add temp classes

---
+++ config/constants.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+define("LIKE_REQUEST_STATUS", 'Request%');
+
+define("STATUS_ALL_REQ", '%Request%');
+
+define("STATUS_REQUEST", "Requested");
+
+define("STATUS_ISSUED", "Issued");
+
+define("STATUS_RETURNED", "Returned");
+
+define("STATUS_REQ_ACCEPTED", "Request Accepted");
+
+define("STATUS_REQ_REJECTED", "Request Rejected");
+
+define("STATUS_DEL_REQ", "Deleted Request");
+
+define("STATUS_REQ", "Request%");
+
+define("DEFAULT_DELETION_TOKEN", 'N/A');
+
+define("ADMIN_USER", "Admin");
+
+define("LIBR_USER", "Librarian");
+
+define("REG_USER", "User");
+
+define("VALID_LOGIN", true);
+
+define("COVER_PIC_PATH", 'book/');
+
+define("DEFAULT_DATE_VAL", '0000-00-00');
---
+++ src/Module/Test/Module.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Module\Home;
+
+class Module extends \System\Core\BaseModule
+{
+    public function __construct()
+    {
+        parent::__construct('test');
+    }
+}
